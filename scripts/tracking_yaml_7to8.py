4e494f25abcb6e1820cd5e1d7e0579ee312ca7cb
Lukas Schrangl <schrangl@iap.tuwien.ac.at>
2018-07-04 22:20:48 +0200

    Add conversion script for tracking yaml v007 -> v008

---
+++ scripts/tracking_yaml_7to8.py
@@ -0,0 +1,24 @@
+import argparse
+from collections import OrderedDict
+
+from sdt import io, fret, chromatic
+
+
+if __name__ == "__main__":
+    ap = argparse.ArgumentParser(
+        description="Convert v007 tracking yaml to v008")
+    ap.add_argument("file", help="YAML file to convert")
+    args = ap.parse_args()
+
+    with open(args.file) as f:
+        data = io.yaml.safe_load(f)
+
+    files = data.pop("files")
+    sources = OrderedDict()
+    for key, f in files.items():
+        sources[key] = {"files": f, "cells": False}
+    data["sources"] = sources
+    data["data_dir"] = ""
+
+    with open(args.file.replace("v007", "v008"), "w") as f:
+        io.yaml.safe_dump(data, f)
