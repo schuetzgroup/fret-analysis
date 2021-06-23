b4ca9600c7403acfa831b4d8a2fb3e1eb1b991d5
Lukas Schrangl <lukas.schrangl@tuwien.ac.at>
2021-06-23 10:18:10 +0200

    Add DataStore as the future way of loading and saving data

---
+++ smfret_analysis/data_store.py
@@ -0,0 +1,338 @@
+# SPDX-FileCopyrightText: 2021 Lukas Schrangl <lukas.schrangl@tuwien.ac.at>
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+from collections import defaultdict
+from pathlib import Path
+import re
+from typing import Dict, Optional
+import warnings
+
+import numpy as np
+import pandas as pd
+from sdt import flatfield as _flatfield, io
+
+
+class DataStore:
+    """Save and load data
+
+    Data is stored as attributes (e.g. :py:attr:`tracks`). Arbitrary
+    attributes can be added and will be serialized to YAML when saving. Some
+    attributes (:py:attr:`localizations`, :py:attr:`tracks`,
+    :py:attr:`flatfield`, :py:attr:`segment_images`) receive special treatment
+    and are saved to binary files.
+
+    Examples
+    --------
+    If ``loc`` is a dictionary mapping dataset ids to localization data, and
+    ``trc`` maps dataset ids to tracking data, save to disk using
+
+    >>> ds = DataStore(localizations=loc, tracks=trc)
+    >>> ds.save()
+
+    Load data using the :py:meth:`load` method, access data as attributes:
+
+    >>> ld = DataStore.load()
+    >>> trc = ld.tracks
+    """
+    localizations: Dict[str, pd.DataFrame]
+    """Maps dataset id -> localization data as created by
+    :py:meth:`Tracker.locate`
+    """
+    tracks: Dict[str, pd.DataFrame]
+    """Maps dataset id -> tracking data as created by
+    :py:meth:`Tracker.track`
+    """
+    flatfield: Dict[str, _flatfield.Corrector]
+    """Maps excitation channel ("donor" or "acceptor") to flatfield Corrector
+    instance.
+    """
+    segment_images: Dict[str, Dict[str, np.ndarray]]
+    """Maps dataset id, file id -> sequence of images used for segmentation"""
+    data_dir: Path
+    """Path to raw data"""
+
+    def __init__(self, **data):
+        """Parameters
+        ----------
+        **data
+            Data to add to class instance as attributes
+        """
+        self.localizations = {}
+        self.tracks = {}
+        self.flatfield = {}
+        self.segment_images = {}
+        self.data_dir = Path(data.pop("data_dir", ""))
+        self.__dict__.update(data)
+
+    def save(self, file_prefix: str = "tracking", file_mode: str = "w"):
+        """Save data to disk
+
+        This will save attributes to disk.
+
+        Parameters
+        ----------
+        file_prefix
+            Common file_prefix for all files written by this method. It will be
+            suffixed by the output format version (v{version}) and file
+            extensions corresponding to what is saved in each file.
+        file_mode
+            Use "w" to delete previously existing HDF5 files (which contain
+            localization and tracking data) and write a new one. As a result,
+            only the current data will end up in the file. Use "a" for
+            appending. In this case, all data with keys that are present
+            in :py:attr:`localizations` or :py:attr:`tracks` will be
+            overwritten, but other data remains in the files.
+            """
+        outfile = Path(f"{file_prefix}-v014")
+        data = self.__dict__.copy()
+
+        with warnings.catch_warnings():
+            import tables
+            warnings.simplefilter("ignore", tables.NaturalNameWarning)
+
+            with pd.HDFStore(outfile.with_suffix(".loc.h5"), file_mode) as s:
+                for key, loc in data.pop("localizations").items():
+                    s.put(key, loc)
+            with pd.HDFStore(outfile.with_suffix(".tracks.h5"),
+                             file_mode) as s:
+                for key, trc in data.pop("tracks").items():
+                    # Categorical exc_type does not allow for storing in fixed
+                    # format while multiindex for both rows and columns does
+                    # not work with table format…
+                    s.put(key, trc.astype({("fret", "exc_type"): str}))
+        seg = {}
+        for k, v in data.pop("segment_images").items():
+            for k2, v2 in v.items():
+                seg[f"{k}/{k2}"] = v2
+        np.savez_compressed(outfile.with_suffix(".seg_img.npz"), **seg)
+        for k, ff in data.pop("flatfield").items():
+            ff.save(outfile.with_suffix(f".flat_{k}.npz"))
+        data["data_dir"] = str(data["data_dir"])
+        with outfile.with_suffix(".yaml").open("w") as f:
+            io.yaml.safe_dump(data, f)
+
+    @classmethod
+    def load(cls, file_prefix: str = "tracking", loc: bool = True,
+             tracks: bool = True, segment_images: bool = True,
+             flatfield: bool = True, version: Optional[int] = None,
+             filtered_file_prefix: str = "filtered"):
+        """Load data to a new class instance
+
+        Parameters
+        ----------
+        file_prefix
+            Prefix used for saving via :py:meth:`save`.
+        loc
+            Whether to load localization data.
+        tracks
+            Whether to load tracking data.
+        cell_images
+            Whether to load cell images.
+        flatfield
+            Whether to load flatfield corrections.
+        filtered_file_prefix
+            Prefix used for saving analyzed and filtered data in v013 format,
+            i.e. prefix passed to the ``save()`` method in the Analysis
+            notebook. v013 only.
+
+        Returns
+        -------
+            Dictionary of loaded data and settings.
+        """
+        if version in (14, None):
+            try:
+                return cls(**cls._load_v14(
+                    file_prefix, loc, tracks, segment_images, flatfield))
+            except FileNotFoundError:
+                if version is not None:
+                    raise
+        if version in (13, None):
+            try:
+                return cls(**cls._load_v13(
+                    file_prefix, loc, tracks, segment_images, flatfield,
+                    filtered_file_prefix))
+            except FileNotFoundError:
+                if version is not None:
+                    raise
+        if version is None:
+            raise ValueError("cannot find supported save files")
+        else:
+            raise ValueError(f"cannot load version {version} save files")
+
+    @staticmethod
+    def _load_v13(file_prefix: str, loc: bool, tracks: bool,
+                  segment_images: bool, flatfield: bool, filtered_file_prefix
+                  ) -> Dict:
+        """Load data to a dictionary (v013 format)
+
+        Parameters
+        ----------
+        file_prefix
+            Prefix used for saving via :py:meth:`save`.
+        loc
+            Whether to load localization data.
+        tracks
+            Whether to load tracking data.
+        cell_images
+            Whether to load cell images.
+        flatfield
+            Whether to load flatfield corrections.
+        filtered_file_prefix
+            Prefix used for saving analyzed and filtered data in v013 format,
+            i.e. prefix passed to the ``save()`` method in the Analysis
+            notebook.
+
+        Returns
+        -------
+            Dictionary of loaded data and settings.
+        """
+        infile = Path(f"{file_prefix}-v013")
+        with infile.with_suffix(".yaml").open() as f:
+            ret = io.yaml.safe_load(f)
+
+        ret["data_dir"] = Path(ret.get("data_dir", ""))
+        ret["localizations"] = {}
+        ret["tracks"] = {}
+        ret["segment_images"] = defaultdict(dict)
+        ret["flatfield"] = {}
+
+        for src in ret["sources"], ret["special_sources"]:
+            for k, files in src.items():
+                src[k] = {i: tuple(f) if isinstance(f, list) else f
+                          for i, f in enumerate(files)}
+
+        all_src = {**ret["sources"], **ret["special_sources"]}
+
+        do_load = []
+        if loc:
+            do_load.append((ret["localizations"], "_loc"))
+        if tracks:
+            do_load.append((ret["tracks"], "_trc"))
+        if len(do_load):
+            with pd.HDFStore(infile.with_suffix(".h5"), "r") as s:
+                for sink, suffix in do_load:
+                    keys = (k for k in s.keys() if k.endswith(suffix))
+                    for k in keys:
+                        new_key = k[1:-len(suffix)]
+                        loaded = s[k]
+                        src = all_src[new_key]
+                        fname_map = pd.Series(src.keys(), index=src.values())
+                        loaded.index = loaded.index.set_levels(
+                            fname_map[loaded.index.levels[0]], level=0)
+                        if suffix == "_trc":
+                            # Restore categorical exc_type. See comment in
+                            # `save` method for details.
+                            loaded = loaded.astype(
+                                {("fret", "exc_type"): "category"})
+                        sink[new_key] = loaded
+
+        if segment_images:
+            seg_img_file = infile.with_suffix(".cell_img.npz")
+            # Map file names to dataset IDs
+            fname_map = {fname: (did, fid)
+                         for did, src in all_src.items()
+                         for fid, fname in src.items()}
+            try:
+                with np.load(seg_img_file) as data:
+                    ci = dict(data)
+                for k, v in ci.items():
+                    k_split = k.split("\n")
+                    if len(k_split) == 1:
+                        new_k = k_split[0]
+                    else:
+                        new_k = tuple(k_split)
+                    did, fid = fname_map[new_k]
+                    ret["segment_images"][did][fid] = v
+            except Exception as e:
+                warnings.warn("Could not load segmentation images from file "
+                              f"\"{str(seg_img_file)}\" ({e}).")
+        if flatfield:
+            flatfield_glob = str(infile.with_suffix(".flat_*.npz"))
+            key_re = re.compile(r"^\.flat_([\w\s-]+)")
+            for p in Path().glob(flatfield_glob):
+                m = key_re.match(p.suffixes[-2])
+                if m is None:
+                    warnings.warn("Could not load flatfield corrector from "
+                                  f"{str(p)}.")
+                else:
+                    ret["flatfield"][m.group(1)] = _flatfield.Corrector.load(p)
+
+        return ret
+
+    @staticmethod
+    def _load_v14(file_prefix: str, loc: bool, tracks: bool,
+                  segment_images: bool, flatfield: bool) -> Dict:
+        """Load data to a dictionary (v014 format)
+
+        Parameters
+        ----------
+        file_prefix
+            Prefix used for saving via :py:meth:`save`.
+        loc
+            Whether to load localization data.
+        tracks
+            Whether to load tracking data.
+        segment_images
+            Whether to load segmentation images.
+        flatfield
+            Whether to load flatfield corrections.
+
+        Returns
+        -------
+            Dictionary of loaded data and settings.
+        """
+        infile = Path(f"{file_prefix}-v014")
+        with infile.with_suffix(".yaml").open() as f:
+            ret = io.yaml.safe_load(f)
+
+        ret["data_dir"] = Path(ret.get("data_dir", ""))
+        ret["localizations"] = {}
+        ret["tracks"] = {}
+        ret["segment_images"] = defaultdict(dict)
+        ret["flatfield"] = {}
+
+        # Restore file tuples which were converted to lists by saving to YAML
+        for src in ret["sources"], ret["special_sources"]:
+            for k, files in src.items():
+                for fid, f in files.items():
+                    if isinstance(f, list):
+                        files[fid] = tuple(f)
+
+        if loc:
+            with pd.HDFStore(infile.with_suffix(".loc.h5"), "r") as s:
+                for k in s.keys():
+                    ret["localizations"][k[1:]] = s.get(k)
+        if tracks:
+            with pd.HDFStore(infile.with_suffix(".tracks.h5"), "r") as s:
+                for k in s.keys():
+                    loaded = s.get(k)
+                    # Restore categorical exc_type. See comment in
+                    # `save` method for details.
+                    ret["tracks"][k[1:]] = loaded.astype(
+                        {("fret", "exc_type"): "category"})
+        if segment_images:
+            seg_img_file = infile.with_suffix(".seg_img.npz")
+            try:
+                with np.load(seg_img_file) as data:
+                    ci = dict(data)
+                for k, v in ci.items():
+                    split_idx = k.rfind("/")
+                    k1 = k[:split_idx]
+                    k2 = k[split_idx+1:]
+                    ret["segment_images"][k1][k2] = v
+            except Exception as e:
+                warnings.warn("Could not load segmentation images from file "
+                              f"\"{str(seg_img_file)}\" ({e}).")
+        if flatfield:
+            flatfield_glob = str(infile.with_suffix(".flat_*.npz"))
+            key_re = re.compile(r"^\.flat_([\w\s-]+)")
+            for p in Path().glob(flatfield_glob):
+                m = key_re.match(p.suffixes[-2])
+                if m is None:
+                    warnings.warn("Could not load flatfield corrector from "
+                                  f"{str(p)}.")
+                else:
+                    ret["flatfield"][m.group(1)] = _flatfield.Corrector.load(p)
+
+        return ret
