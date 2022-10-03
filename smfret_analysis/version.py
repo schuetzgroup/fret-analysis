ea8641ad01b439b60f16bfbae1f8f5a360dede79
Lukas Schrangl <lukas.schrangl@tuwien.ac.at>
2022-10-03 11:11:30 +0200

    Bump version to 4.0.dev

--- smfret_analysis/version.py
+++ smfret_analysis/version.py
@@ -2,21 +2,11 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
-import subprocess
-
-import sdt
-
-
-__version__ = "3.0"
+__version__ = "4.0.dev"
 
 
 def print_info():
-    try:
-        git_rev = subprocess.check_output(["git", "describe", "--always"])
-        git_rev = git_rev.decode().strip()
-    except Exception:
-        git_rev = "unknown"
+    import sdt
 
     print(f"""smFRET analysis software version {__version__}
-(git revision {git_rev})
 Using sdt-python version {sdt.__version__}""")
